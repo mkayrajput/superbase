54521e4b6e557cb602af1316df0c19f881847c04
Mahender Kumar <mkayrajput8@gmail.com>
2025-11-01 21:28:03 +0530

    DB setup with Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
+    <div className="min-h-screen min-w-screen flex items-center justify-center ">
       <Button>
         Click Me
       </Button>
