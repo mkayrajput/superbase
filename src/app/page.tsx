d69426cc81d54a2eccca362c60974571566908ed
Mahender Kumar <mkayrajput8@gmail.com>
2025-11-24 23:44:11 +0530

    inngest configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import { requireAuth } from "@/lib/auth-utils";
+"use client";
 
-const Home = async () => {
-  await requireAuth();
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+const Home = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
+
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center"></div>
+    <div className="min-h-screen min-w-screen flex flex-col items-center justify-center">
+      {JSON.stringify(data)}
+      <Button onClick={() => create.mutate()} disabled={create.isPending}>
+        Create Workflow
+      </Button>
+    </div>
   );
 };
 
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,27 @@
-import { z } from "zod";
+import { inngest } from "@/inngest/client";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import prisma from "@/lib/db";
+
+
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+
+  createWorkflow: protectedProcedure.mutation(async() => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "mkayrajput@example.com"
+      }
+    })
+    return prisma.workflow.create({
+      data: {
+        name: "text-worKflow",
+      },
+    });
+  }),
 });
+
 // export type definition of API
 export type AppRouter = typeof appRouter;
