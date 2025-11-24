0add597b32589fc5ba6dba111c71666edfe86a05
Mahender Kumar <mkayrajput8@gmail.com>
2025-11-25 00:35:58 +0530

    gemini ai integration

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,19 +2,29 @@ import { inngest } from "@/inngest/client";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 
-
 export const appRouter = createTRPCRouter({
+  testAi: baseProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return {
+      success: true,
+      message: "Job queued",
+    };
+  }),
+  
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
 
-  createWorkflow: protectedProcedure.mutation(async() => {
+  createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
       name: "test/hello.world",
       data: {
-        email: "mkayrajput@example.com"
-      }
-    })
+        email: "mkayrajput@example.com",
+      },
+    });
     return prisma.workflow.create({
       data: {
         name: "text-worKflow",
